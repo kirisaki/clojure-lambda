568ac05ef47cf0dfda506665a6b9c4bb04905ce6
Akihito Kirisaki <akirisaki@seqsense.com>
2021-09-25 22:54:19 +0900

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-lambda "0.1.0-SNAPSHOT"
+  :license {:name "Unlisence"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns clojure-lambda.core})
---
+++ test/clojure_lambda/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-lambda.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-lambda.core :refer :all]))
+
+(deftest show-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
